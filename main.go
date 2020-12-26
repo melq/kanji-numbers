6b7a7ead434b375a96ef5e09567f4af6b3b518bb
melq <tu2sice@yahoo.co.jp>
2020-12-26 13:47:37 +0900

    :construction: 各変換関数を受け取った文字そのまま返す形で用意

--- main.go
+++ main.go
@@ -14,11 +14,28 @@ func returnHello(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(n, err)
 	}
 }
+func number2kanji(w http.ResponseWriter, r *http.Request) {
+	log.Println("number2kanji called")
+	vars := mux.Vars(r)
+	if n, err := fmt.Fprint(w, vars["number"]); err != nil {
+		log.Fatal(n, err)
+	}
+}
+
+func kanji2number(w http.ResponseWriter, r *http.Request) {
+	log.Println("kanji2number called")
+	vars := mux.Vars(r)
+	if n, err := fmt.Fprint(w, vars["kanji"]); err != nil {
+		log.Fatal(n, err)
+	}
+}
 
 func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/hello", returnHello).Methods("GET")
+	router.HandleFunc("/v1/number2kanji/{number}", number2kanji).Methods("GET")
+	router.HandleFunc("/v1/kanji2number/{kanji}", kanji2number).Methods("GET")
 
 	log.Println("Listen Server ...")
 	log.Fatal(http.ListenAndServe(":8000", router))
