c371c87ed907f663d61d09e8ad8ab4ce0e4256e0
melq <tu2sice@yahoo.co.jp>
2020-12-26 16:20:12 +0900

    :construction: 拾百千を追記出来るよう変更

--- main.go
+++ main.go
@@ -3,22 +3,48 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"strconv"
-
-	"github.com/gorilla/mux"
 )
 
+func singleTrans(input rune) (output rune) {
+	switch input {
+	case '0': output = '零'
+	case '1': output = '壱'
+	case '2': output = '弐'
+	case '3': output = '参'
+	case '4': output = '四'
+	case '5': output = '五'
+	case '6': output = '六'
+	case '7': output = '七'
+	case '8': output = '八'
+	case '9': output = '九'
+	}
+	return
+}
+
 func number2kanji(numStr string) (kanji string, err error) {
-	var num int64
-	num, err = strconv.ParseInt(numStr, 10, 64)
-	if err != nil { return }
-	if num > 9999999999999999 {
-		err = errors.New("number2kanji: parsing \"" + strconv.FormatInt(num, 10) + "\": value out of range")
+	kanji = numStr
+	if len(numStr) > 16 {
+		err = errors.New("number2kanji: parsing \"" + numStr + "\": value out of range")
 	}
 
-	kanji = "kanji: " + strconv.FormatInt(num, 10)
+	var runes []rune
+	littleUnits := []rune{'千', '_', '拾', '百'}
+	indexLittleUnits := len(numStr) % 4
+	//bigUnits := []rune{'万', '億', '兆'}
+	for _, c := range numStr {
+		if indexLittleUnits == 1 {
+			runes = append(runes, singleTrans(c))
+		} else {
+			runes = append(runes, singleTrans(c), littleUnits[indexLittleUnits])
+		}
+		if indexLittleUnits == 0 { indexLittleUnits = 3
+		} else { indexLittleUnits-- }
+	}
+
+	kanji = string(runes)
 	return
 }
 
