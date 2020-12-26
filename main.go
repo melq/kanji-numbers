b604e5d6935475e3834e42adcf2b3fe83407c52b
melq <tu2sice@yahoo.co.jp>
2020-12-26 15:05:46 +0900

    :construction: number2kanji のエラー処理を記述

--- main.go
+++ main.go
@@ -1,41 +1,63 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-func returnHello(w http.ResponseWriter, r *http.Request) {
-	log.Println("returnHello called")
-	if n, err := fmt.Fprint(w, "hello world!"); err != nil {
-		log.Fatal(n, err)
+func number2kanji(numStr string) (kanji string, err error) {
+	var num int64
+	num, err = strconv.ParseInt(numStr, 10, 64)
+	if err != nil { return }
+	if num > 9999999999999999 {
+		err = errors.New("number2kanji: parsing \"" + strconv.FormatInt(num, 10) + "\": value out of range")
 	}
+
+	kanji = "kanji: " + strconv.FormatInt(num, 10)
+	return
 }
-func number2kanji(w http.ResponseWriter, r *http.Request) {
-	log.Println("number2kanji called")
+
+func kanji2number(kanji string) (numStr string) {
+	numStr = "1234"
+	return
+}
+
+func handleNumber2kanji(w http.ResponseWriter, r *http.Request) {
+	log.Println("handleNumber2kanji called")
+	var kanji string
+
 	vars := mux.Vars(r)
-	if n, err := fmt.Fprint(w, vars["number"]); err != nil {
-		log.Fatal(n, err)
+	kanji, err := number2kanji(vars["number"])
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	if _, err := fmt.Fprint(w, kanji); err != nil {
+		log.Fatal(err)
 	}
 }
 
-func kanji2number(w http.ResponseWriter, r *http.Request) {
-	log.Println("kanji2number called")
+func handleKanji2number(w http.ResponseWriter, r *http.Request) {
+	log.Println("handleKanji2number called")
 	vars := mux.Vars(r)
-	if n, err := fmt.Fprint(w, vars["kanji"]); err != nil {
-		log.Fatal(n, err)
+
+	if _, err := fmt.Fprint(w, vars["kanji"]); err != nil {
+		log.Fatal(err)
 	}
 }
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/hello", returnHello).Methods("GET")
-	router.HandleFunc("/v1/number2kanji/{number}", number2kanji).Methods("GET")
-	router.HandleFunc("/v1/kanji2number/{kanji}", kanji2number).Methods("GET")
+	router.HandleFunc("/v1/number2kanji/{number}", handleNumber2kanji).Methods("GET")
+	router.HandleFunc("/v1/kanji2number/{kanji}", handleKanji2number).Methods("GET")
 
 	log.Println("Listen Server ...")
 	log.Fatal(http.ListenAndServe(":8000", router))
